d64dc8e9421ff2af688ba2c008a13f415044a1c9
Rainer Koirikivi <rainer@koirikivi.fi>
2021-11-23 18:58:50 +0200

    Enable solidity optimization

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -33,6 +33,12 @@ export default {
     compilers: [
       {
         version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
       },
     ]
   },
