f6445e77494d311b5ade7e3693469e929f1cca54
Rainer Koirikivi <rainer@koirikivi.fi>
2021-12-05 16:45:17 +0200

    Update comments and readme

--- contracts/PayRueStaking.sol
+++ contracts/PayRueStaking.sol
@@ -3,9 +3,10 @@
 * The PayRue Staking Contract
 *
 * Features and assumptions:
-* - Users stake PROPEL and also receive PROPEL (though it also supports other tokens with 1:1 reward ratio)
-* - APY is always 100% - you stake 10 000 PROPEL, you get 10 000 PROPEL as rewards during the next year
-* - Each stake is guaranteed the 100% reward in 365 days, after which they can still get new rewards if
+* - Users stake token A and receive token B. These can be same or different tokens.
+* - APY is configurable with rewardNumerator/rewardDenominator -- with 1 and 1 it's 100%, which means
+    you stake 10 000 PROPEL, you get 10 000 PROPEL as rewards during the next year
+* - Each stake is guaranteed the reward in 365 days, after which they can still get new rewards if
 *   there is reward money left in the contract. If the reward cannot be guaranteed, the stake will not be accepted.
 * - Each stake is locked for 365 days, after which it can be unstaked or left in the contract
 */
