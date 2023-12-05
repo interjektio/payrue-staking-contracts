d00611fb226917f5966c880384e07c4474fc6963
longnguyen <long.nguyendr@gmail.com>
2023-12-05 15:05:35 +0200

    Update model: Add relationship, and, KeyValuePair model.

--- python/staking_rewarder/models/__init__.py
+++ python/staking_rewarder/models/__init__.py
@@ -1,21 +1,27 @@
 from .metadata import Base, metadata
-from .rewards import RewardDistribution, DistributionRound, RewardState
-from sqlalchemy.orm import sessionmaker
+from .rewards import RewardDistribution, DistributionRound, RewardState, KeyValuePair
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import create_engine
 
 __all__ = [
-    'Base',
-    'metadata',
-    'RewardDistribution',
-    'DistributionRound',
-    'RewardState'
+    "Base",
+    "metadata",
+    "RewardDistribution",
+    "DistributionRound",
+    "RewardState",
+    "KeyValuePair",
+    "db_session",
+    "autocommit_engine",
+    "Session",
 ]
 
 from .. import config
 
 """Initialize database tables."""
-db_connection_string = config['database']['sqlalchemy.url']
+db_connection_string = config["database"]["sqlalchemy.url"]
 
 engine = create_engine(db_connection_string)  # Use connection string from config
 Session = sessionmaker(bind=engine)
 db_session = Session()
+
+autocommit_engine = engine.execution_options(isolation_level="AUTOCOMMIT")
--- python/staking_rewarder/models/rewards.py
+++ python/staking_rewarder/models/rewards.py
@@ -1,30 +1,56 @@
 import enum
 
-from sqlalchemy import (Column, Integer, String, Float, DOUBLE_PRECISION, Enum, ForeignKey, BigInteger)
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Float,
+    DOUBLE_PRECISION,
+    Enum,
+    ForeignKey,
+    BigInteger,
+)
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import relationship
 from .metadata import Base
+from .types import Uint256
 
 
 class RewardState(enum.Enum):
     sent = "sent"
-    pending = "pending"
+    sending = "sending"
     unsent = "unsent"
 
 
 class RewardDistribution(Base):
-    __tablename__ = 'reward_distribution'
+    __tablename__ = "reward_distribution"
     id = Column(Integer, primary_key=True)
     user_address = Column(String)
     percentage = Column(DOUBLE_PRECISION)
-    amount_wei = Column(BigInteger)
-    state = Column(Enum(RewardState), nullable=False, default=RewardState.unsent, server_default="unsent")
-    distribution_round_id = Column(Integer, ForeignKey('distribution_round.id'))
-    distribution_round = relationship("DistributionRound", back_populates="reward_distributions")
+    amount_wei = Column(Uint256, nullable=False)
+    state = Column(
+        Enum(RewardState),
+        nullable=False,
+        default=RewardState.unsent,
+        server_default="unsent",
+    )
+    distribution_round_id = Column(Integer, ForeignKey("distribution_round.id"))
+    distribution_round = relationship(
+        "DistributionRound", back_populates="reward_distributions"
+    )
 
 
 class DistributionRound(Base):
-    __tablename__ = 'distribution_round'
+    __tablename__ = "distribution_round"
     id = Column(Integer, primary_key=True)
     year = Column(Integer)
     month = Column(Integer)
-    reward_distributions = relationship("RewardDistribution", back_populates="distribution_round")
+    reward_distributions = relationship(
+        "RewardDistribution", back_populates="distribution_round"
+    )
+
+
+class KeyValuePair(Base):
+    __tablename__ = "key_value_pair"
+    key = Column(String, primary_key=True)
+    value = Column(JSONB)
